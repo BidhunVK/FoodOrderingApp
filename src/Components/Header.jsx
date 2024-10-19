3993325f3ef975ce5f0f515969dad6d171304d32
Bidhun <bidhunvaikkath@gmail.com>
2024-10-19 11:11:31 +0530

    Display number of items in the cart

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,23 @@
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
+import CartContext from '../store/CartContext';
+import { useContext } from 'react';
 
 export default function Header(){
+
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems , item)=>{
+        return totalNumberOfItems + item.quantity;
+    }, 0)
+    
     return <header id="main-header">
         <div id="title">
             <img src={logoImg} alt="A restaurant" />
             <h1>ReactFood</h1>
         </div>
         <nav>
-            <Button textOnly>Cart (0)</Button>
+            <Button textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
 }
