aba024e664363fcebec907440b0499bd9036ccdd
Bidhun <bidhunvaikkath@gmail.com>
2024-10-19 10:39:07 +0530

    remove item from the cart logic in CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./Components/Header";
 import Meals from "./Components/Meals";
 
+import { CartContextProvider } from "./store/CartContext";
+
 function App() {
   return (
     <>
-     <Header />
-     <Meals />
+      <CartContextProvider>
+        <Header />
+        <Meals />
+      </CartContextProvider>
     </>
   );
 }
